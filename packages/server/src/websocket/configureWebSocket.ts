66140aed584ea5163374622d0f3e39b0b9ffbb91
Sam Heutmaker <samheutmaker@gmail.com>
2022-06-06 12:01:56 -0500

    make redis optional for socket.io

--- packages/server/src/websocket/configureWebSocket.ts
+++ packages/server/src/websocket/configureWebSocket.ts
@@ -3,15 +3,22 @@ import { RedisClientType } from 'redis';
 import { Server as SocketServer } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
 
-export const configureWebSocket = async (server: Server, redis: RedisClientType): Promise<SocketServer> => {
+export const configureWebSocket = async (server: Server, redis?: RedisClientType): Promise<SocketServer> => {
   const io = new SocketServer(server, {
     cors: {
       origin: '*',
       methods: ['GET', 'PUT', 'POST', 'DELETE'],
     },
   });
-  const subClient = redis.duplicate();
-  await Promise.all([redis.connect(), subClient.connect()]);
-  io.adapter(createAdapter(redis, subClient));
+  /**
+   * Redis is optional when configing the socket.io connection
+   * This will typically be used when running in a multi-process environment
+   * and is not required for development.
+   */
+  if (redis) {
+    const subClient = redis.duplicate();
+    await Promise.all([redis.connect(), subClient.connect()]);
+    io.adapter(createAdapter(redis, subClient));
+  }
   return io;
 };
