8436c00d696027d6d2898f6c9d4b8ae58718231f
Sam Heutmaker <samheutmaker@gmail.com>
2022-06-08 12:17:51 -0500

    move marketing site into separate repo

--- packages/marketing/src/env.ts
+++
@@ -1,6 +0,0 @@
-export const NODE_ENV = process.env.NODE_ENV || 'development';
-export const DEBUG_ENABLED = process.env.DEBUG_ENABLED || 1;
-export const WEB_UI_URL = process.env.WEB_UI_URL || 'http://localhost:3000';
-export const PAY_PORTAL_URL = process.env.PAY_PORTAL_URL || 'http://localhost:3004';
-export const API_URL = process.env.API_URL || 'https://magically-production.ngrok.io';
-export const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID || 'G-43HKQKDVV2';
