9f2ef3ea168adc3fd0396de68abbe3783fbebb1e
Sam Heutmaker <samheutmaker@gmail.com>
2022-07-27 21:08:26 -0600

    fix env var injection

--- packages/server/src/utils/RPCConnection.ts
+++ packages/server/src/utils/RPCConnection.ts
@@ -1,8 +1,8 @@
 import { Connection } from '@solana/web3.js';
-// import { SOLANA_RPC_ENDPOINT } from '../env';
+import { SOLANA_RPC_ENDPOINT } from '../env';
 
 const RPCConnection = new Connection(
-  'https://solana-api.syndica.io/access-token/j8ZlHMcxVzTA5r5k51Q7WI4IrZB1ZURNMvjswD7U6NxpKUDUIIeHTHHnXogW8v2J/rpc',
+  SOLANA_RPC_ENDPOINT,
   'confirmed',
 );
 
